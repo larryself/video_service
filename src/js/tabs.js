904dcd3a6b1af3d8da4bc722e9975020c5ce4a10
Yan <yanmusikhin@gmail.com>
2021-07-27 13:32:32 +0400

    changed tabs/add elm

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -18,15 +18,17 @@ tabsButton.forEach(tab => {
 function openActiveElement(trig){
     const tabs = [...document.querySelectorAll(`[data-trigger]`)];
     const section = [...document.querySelectorAll(`[data-content]`)];
+    const sectionActiveClass ='main-nav__section_active';
+    const itemActiveClass = 'main-nav__item_active';
     tabs.forEach((tab, i) => {
         if (tab.dataset.trigger != trig) {
-            if (tab.classList.contains('main-nav__item_active')) {
-                tab.classList.remove('main-nav__item_active');
-                section[i].classList.remove('main-nav__section_active');
+            if (tab.classList.contains(itemActiveClass)) {
+                tab.classList.remove(itemActiveClass);
+                section[i].classList.remove(sectionActiveClass);
             }
         } else {
-            tab.classList.add('main-nav__item_active');
-            section[i].classList.add('main-nav__section_active');
+            tab.classList.add(itemActiveClass);
+            section[i].classList.add(sectionActiveClass);
             window.location.hash = trig;
         }
     })
